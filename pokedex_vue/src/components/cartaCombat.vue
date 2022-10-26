97c61e3cc498addb8f485374b6218eab2cb3e2b0
saimonblu <simon.montoya.97@lacetania.cat>
2022-10-26 20:00:02 +0200

    26/10/2022 - theme

--- pokedex_vue/src/components/cartaCombat.vue
+++ pokedex_vue/src/components/cartaCombat.vue
@@ -51,44 +51,5 @@
         }
 
       }
-
   }
 </script>
-
-<style>
-
-  .flip-card {
-    background-color: transparent;
-    perspective: 1000px;
-  }
-
-  .flip-card-inner {
-    position: relative;
-    width: 100%;
-    height: 100%;
-    text-align: center;
-    transition: transform 0.6s;
-    transform-style: preserve-3d;
-    box-shadow: 0 4px 8px 0 rgba(0,0,0,0.2);
-  }
-
-  .flip-card-front, .flip-card-back {
-    position: absolute;
-    width: 100%;
-    height:250px;
-    -webkit-backface-visibility: hidden;
-    backface-visibility: hidden;
-  }
-
-  .flip-card-front {
-    background-color: #bbb;
-    color: black;
-  }
-
-  .flip-card-back {
-    /*background-color: transparent;*/
-    color: white;
-    transform: rotateY(180deg);
-    margin-top: -265px;
-  }
-</style>
